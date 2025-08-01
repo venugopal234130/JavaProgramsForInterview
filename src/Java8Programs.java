474cc0f02947598f47629aade18c104835193249
panchalavenugopal <venumail13@gmail.com>
2025-08-02 01:44:48 +0530

    git commit -m "ThirdCommit"

--- src/Java8Programs.java
+++ src/Java8Programs.java
@@ -11,8 +11,11 @@ public class Java8Programs {
         int arr[]={10,2,4,5,1,6,8};
         List<Integer> DescendingOrder=Arrays.stream(arr).boxed().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
         System.out.println(DescendingOrder);
+        //Convert list to Map
         List<String> strings=Arrays.asList("venu","gopal");
         Map<String,Integer> toMap=strings.stream().collect(Collectors.toMap(String::toUpperCase,String::length));
         System.out.println(toMap);
+        //Filter the even numbers
+        Arrays.stream(arr).boxed().filter(n->n%2==0).forEach(System.out::println);
     }
 }
